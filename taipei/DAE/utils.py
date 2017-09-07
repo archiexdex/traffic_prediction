cca7346832cfaace345fbf9335c07ea10813c71a
jay <jay_case@hotmail.com>
2017-09-07 21:11:39 +0800

    utils minor bugs fixed

--- taipei/DAE/utils.py
+++ taipei/DAE/utils.py
@@ -6,37 +6,32 @@ from __future__ import print_function
 import json
 import numpy as np
 
+
 class Norm(object):
     """
     TODO
     """
+
     def __init__(self):
         self.key = ["time", "density", "flow", "speed", "week"]
         self.norm = {}
         with open("/home/xdex/Desktop/traffic_flow_detection/taipei/training_data/new_raw_data/vd_base/norm.json", 'r') as fp:
             self.norm = json.load(fp)
 
-    def data_normalization(self, data, file_name):
+    def data_normalization(self, data):
         # and dump each pair(mean, std) to json for testing
         for i in range(5):
-            # temp_mean = np.mean(data[:, :, :, i])
-            # temp_std = np.std(data[:, :, :, i])
-            temp_mean = self.norm[file_name][self.key[i]][0]
-            temp_std = self.norm[file_name][self.key[i]][1]
+            temp_mean = self.norm['train'][self.key[i]][0]
+            temp_std = self.norm['train'][self.key[i]][1]
             data[:, :, :, i] = (data[:, :, :, i] - temp_mean) / temp_std
             print(i, temp_mean, temp_std)
-            self.norm[file_name][self.key[i]] = [temp_mean, temp_std]
         return data
 
-
-    def data_recover(self, data, file_name):
+    def data_recover(self, data):
         # and dump each pair(mean, std) to json for testing
-        for i in range(5):
-            # temp_mean = np.mean(data[:, :, :, i])
-            # temp_std = np.std(data[:, :, :, i])
-            temp_mean = self.norm[file_name][self.key[i]][0]
-            temp_std = self.norm[file_name][self.key[i]][1]
-            data[:, :, :, i] = (data[:, :, :, i] - temp_mean) / temp_std
-            print(i, temp_mean, temp_std)
-            self.norm[file_name][self.key[i]] = [temp_mean, temp_std]
+        for i in range(1, 4):
+            temp_mean = self.norm['train'][self.key[i]][0]
+            temp_std = self.norm['train'][self.key[i]][1]
+            data[i-1] = data[i-1] * temp_std + temp_mean
+            # print(i, temp_mean, temp_std)
         return data
